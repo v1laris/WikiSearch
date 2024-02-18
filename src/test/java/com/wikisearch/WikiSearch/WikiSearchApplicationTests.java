157934fce6be5baca012ef4857bc77e8ea99a11f
d1ma <shpakdmitry1@gmail.com>
2024-02-18 15:14:55 +0300

    Initial commit

---
+++ src/test/java/com/wikisearch/WikiSearch/WikiSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikisearch.WikiSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
