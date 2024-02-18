56aa5b5a496e8a9bd96f260b79379ffd3f7c1065
d1ma <shpakdmitry1@gmail.com>
2024-02-18 15:31:49 +0300

    test

--- src/main/java/com/wks/wikisearch/WikiSearchApplication.java
+++ src/main/java/com/wks/wikisearch/WikiSearchApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikiSearchApplication {
 
+	// hui
 	public static void main(String[] args) {
 		SpringApplication.run(WikiSearchApplication.class, args);
 	}
